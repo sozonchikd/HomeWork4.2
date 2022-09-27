5cab3247a2a4f4ad55ffc7cafe64b222ddc1b218
Sozonchik <den.sozon@mail.ru>
2022-09-27 16:20:44 +0500

    выполнил задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием.");
+        }
+        else  {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
+        }
+
     }
 }
