911a67b4a286f6469bc261f8c625fb931aa2aac4
Sozonchik <den.sozon@mail.ru>
2022-09-27 16:27:46 +0500

    выполнил задание 2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,18 @@ public class Main {
         else  {
             System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
         }
-
+        System.out.println("Задание 2");
+        int humanAge = 15;
+        if (humanAge >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        } else if (humanAge >=18) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        else if (humanAge >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        else if (humanAge <7) {
+            System.out.println("Не проходит критерии");
+        }
     }
 }
